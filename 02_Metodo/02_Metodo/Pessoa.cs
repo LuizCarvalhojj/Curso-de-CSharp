de10c1a1d80db94b88a9fa694577aa8588d5892c
Luiz Carvalho <luiz_jaja@hotmail.com>
2017-11-22 21:44:38 -0200

    Aula 2 revisando métodos.

---
+++ 02_Metodo/02_Metodo/Pessoa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Metodo
+{
+    class Pessoa
+    {
+        public string Nome;
+        public string Sexo;
+        public DateTime DataNasc;
+        public double PosicaoX, PosicaoY;
+
+        public string GetPosicao()
+        {
+            return PosicaoX + ", " + PosicaoY;
+        }
+
+        public void SetPosicao (double x, double y)
+        {
+            PosicaoX = x;
+            PosicaoY = y;
+        }
+    }
+}
---
+++ 02_Metodo/02_Metodo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Metodo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa = new Pessoa();
+            pessoa.SetPosicao(10.4, 20.6);
+
+            Console.Write("Posiçâo: " + pessoa.GetPosicao());
+
+            Console.ReadKey();
+        }
+    }
+}
