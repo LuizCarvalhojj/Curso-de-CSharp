f9cbb7d9e4ff02e50f858558ef83a6d637581844
Luiz Carvalho <luiz_jaja@hotmail.com>
2017-11-22 21:31:51 -0200

    Aula 1 revisando classes.

---
+++ Solucao/01_Classe/Pessoa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Classe
+{
+    class Pessoa
+    {
+        public string Nome;
+        public string Sexo;
+        public DateTime DataNascimento;
+
+        public string ResumoPessoa()
+        {
+            string Resumo = "Nome: " + Nome;
+            Resumo += "\nSexo: " + Sexo;
+            Resumo += "\nData de nascimento: " + DataNascimento;
+
+            return Resumo;
+
+        }
+    }
+}
---
+++ Solucao/01_Classe/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Classe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa = new Pessoa();
+            pessoa.Nome = "Luiz Felipe";
+            pessoa.Sexo = "M";
+            pessoa.DataNascimento = new DateTime(1996, 11, 06);
+
+            string texto = pessoa.ResumoPessoa();
+
+            Console.Write(texto);
+
+            Console.ReadKey();
+
+        }
+    }
+}
