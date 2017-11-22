8b027cf840b780edd07b0b23f3b487fa3962102d
Luiz Carvalho <luiz_jaja@hotmail.com>
2017-11-22 21:57:43 -0200

    Revisão construtores

---
+++ 03_Construtores/03_Construtores/Pessoa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Construtores
+{
+    class Pessoa
+    {
+        public string Nome;
+        public string Sexo;
+        DateTime DataNasc;
+        double PosicaoX, PosicaoY;
+
+        public Pessoa(string nome, string sexo, DateTime dataNasc, double x, double y)
+        {
+            Nome = nome;
+            Sexo = sexo;
+            DataNasc = dataNasc;
+            PosicaoX = x;
+            PosicaoY = y;
+        }
+    }
+}
---
+++ 03_Construtores/03_Construtores/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Construtores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa = new Pessoa("Luiz", "M", DateTime.Now, 20.5, 10.3);
+        }
+
+        
+    }
+}
